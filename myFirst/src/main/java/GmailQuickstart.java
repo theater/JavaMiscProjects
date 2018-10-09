0d461b43c7c6daa959c677ed8291c6abad3df288
I032608 <I032608@SOFN34263687A.emea.global.corp.sap>
2018-10-09 09:34:13 +0300

    ...

---
+++ myFirst/src/main/java/GmailQuickstart.java
@@ -0,0 +1,115 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.ListMessagesResponse;
+import com.google.api.services.gmail.model.Message;
+
+public class GmailQuickstart {
+    private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.MAIL_GOOGLE_COM);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = GmailQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+    }
+
+    public static void main(String... args) throws IOException, GeneralSecurityException, MessagingException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // Print the labels in the user's account.
+        String user = "me";
+        ListMessagesResponse listResponse = service.users().messages().list(user).execute();
+        List<Message> messages = listResponse.getMessages();
+        if (messages == null || messages.isEmpty()) {
+            System.out.println("No mails found.");
+        } else {
+            System.out.println("Messages:");
+            for (Message message : messages) {
+            	MimeMessage mimeMessage = getMimeMessage(service, user, message.getId());
+//            	MimeMessageParser parser = new MimeMessageParser(mimeMessage);
+//            	parser.parse();
+            	
+            	mimeMessage.getContent();
+            	System.out.println("Message: " + mimeMessage.getFrom().);
+            	
+            }
+        }
+    }
+    
+    /**
+     * Get a Message and use it to create a MimeMessage.
+     *
+     * @param service Authorized Gmail API instance.
+     * @param userId User's email address. The special value "me"
+     * can be used to indicate the authenticated user.
+     * @param messageId ID of Message to retrieve.
+     * @return MimeMessage MimeMessage populated from retrieved Message.
+     * @throws IOException
+     * @throws MessagingException
+     */
+    public static MimeMessage getMimeMessage(Gmail service, String userId, String messageId)
+        throws IOException, MessagingException {
+      Message message = service.users().messages().get(userId, messageId).setFormat("raw").execute();
+
+      Base64 base64Url = new Base64(true);
+      byte[] emailBytes = base64Url.decodeBase64(message.getRaw());
+
+      Properties props = new Properties();
+      Session session = Session.getDefaultInstance(props, null);
+
+      MimeMessage email = new MimeMessage(session, new ByteArrayInputStream(emailBytes));
+
+      return email;
+    }
+
+}
