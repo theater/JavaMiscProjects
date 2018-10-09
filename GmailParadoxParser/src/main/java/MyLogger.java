4483d6c7c68f570cac432fccc9f2e942c4b5c9a1
theater <theater@BEDROOM-PC>
2018-10-09 19:11:08 +0300

    Renamed root name of the project

---
+++ GmailParadoxParser/src/main/java/MyLogger.java
@@ -0,0 +1,19 @@
+
+public class MyLogger {
+	private static MyLogger logger;
+
+	private MyLogger() {
+
+	}
+
+	public static MyLogger getInstance() {
+		if (logger == null) {
+			logger = new MyLogger();
+		}
+		return logger;
+	}
+
+	public void logDebug(String message) {
+		System.out.println("DEBUG: " + message);
+	}
+}
