093c54f2196f797377bcb4a161d12940b04bda2f
i032608 <i032608@SOFN34263687A.emea.global.corp.sap>
2018-11-13 09:03:30 +0200

    Migrated to maven project for easier dependencies management

--- TCPToParadox/src/mainApp/Main.java
+++ TCPToParadox/src/mainApp/Main.java
@@ -1,9 +1,7 @@
 package mainApp;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -12,39 +10,43 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
-	private static final String PASSWORD_FILE = "resources/password.txt";
+    private static final String PASSWORD_FILE = "resources/password.txt";
 
-	private static Logger logger = LoggerFactory.getLogger(Main.class);
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-	private static final String IP_ADDRESS = "192.168.254.231";
-	private static final int PORT = 10000;
-	private static final String PASSWORD = retrievePassword(PASSWORD_FILE);
+    private static final String IP_ADDRESS = "192.168.254.231";
+    private static final int PORT = 10000;
+    private static final String PASSWORD = retrievePassword(PASSWORD_FILE);
 
-	public static void main(String[] args) throws Exception {
-		ParadoxSystem paradoxSystem = new ParadoxSystem(IP_ADDRESS, PORT, PASSWORD);
-		paradoxSystem.logonSequence();
-		List<String> partitionLabels = paradoxSystem.readPartitions();
-//		List<String> zoneLabels = paradoxSystem.readZones();
+    public static void main(String[] args) {
+        try {
+            ParadoxSystem paradoxSystem = new ParadoxSystem(IP_ADDRESS, PORT, PASSWORD);
+            paradoxSystem.logonSequence();
+            List<String> partitionLabels = paradoxSystem.readPartitions();
+            // List<String> zoneLabels = paradoxSystem.readZones();
 
-		paradoxSystem.logoutSequence();
-		paradoxSystem.close();
+            paradoxSystem.logoutSequence();
+            paradoxSystem.close();
 
-		partitionLabels.stream().forEach(a -> logger.debug("Partition label: " + a));
-		logger.debug("############################################################################");
-//		zoneLabels.stream().forEach(a -> System.out.println("Zone label: " + a));
-	}
+            partitionLabels.stream().forEach(a -> logger.debug("Partition label: " + a));
+            logger.debug("############################################################################");
+            // zoneLabels.stream().forEach(a -> System.out.println("Zone label: " + a));
+        } catch (Exception e) {
+            logger.error("Exception: {}", e.getMessage(), e);
+        }
+    }
 
-	private static String retrievePassword(String file) {
-		try {
-			byte[] bytes = Files.readAllBytes(Paths.get(file));
-			if (bytes != null && bytes.length > 0) {
-				String result = new String(bytes);
-				logger.debug("Password: {}", result);
-				return result;
-			}
-		} catch (IOException e) {
-			logger.debug("Exception during reading password from file", e);
-		}
-		return "";
-	}
+    private static String retrievePassword(String file) {
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(file));
+            if (bytes != null && bytes.length > 0) {
+                String result = new String(bytes);
+                logger.debug("Password: {}", result);
+                return result;
+            }
+        } catch (IOException e) {
+            logger.debug("Exception during reading password from file", e);
+        }
+        return "";
+    }
 }
