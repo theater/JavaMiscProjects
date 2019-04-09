38bec422d944ce20d1e6304b741eb7f8e1b8e21d
i048391 <rosen.rusev@sap.com>
2019-04-09 18:14:35 +0300

    Add Unit Stats

---
+++ DamageCalculator/src/main/java/config/UnitStats.java
@@ -0,0 +1,32 @@
+package main.java.config;
+
+public class UnitStats {
+	private int attack;
+	private int defense;
+	private int health;
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+}
