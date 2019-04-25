612c2c9d714a801df70b04e5846cffe0719b329c
Konstantin Polihronov <polychronov@gmail.com>
2019-04-25 19:36:50 +0300

    Side navigation added - NOT LOADING PAGES IN CONTENT SECTION.
    Signed-off-by: Konstantin Polihronov <polychronov@gmail.com>

--- DamageCalculator/src/main/resources/static/controller/App.controller.js
+++ DamageCalculator/src/main/resources/static/controller/App.controller.js
@@ -3,6 +3,17 @@ sap.ui.define([ "sap/ui/core/mvc/Controller", "sap/m/MessageToast" ], function(
 	"use strict";
 
 	return Controller.extend("DamageCalculator.controller.App", {
+		wolfCalcPress : function() {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("wolf");
+		},
+		wolfCalcJsonPress : function() {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("wolfJson");
+		},
+		battleCalcPress : function() {
+			MessageToast.show("Handle go to Battle calculator here...");
+		}
 	});
 
 });
